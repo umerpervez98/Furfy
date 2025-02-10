5d2f41740e4aff2fb6e4d65509d4c42a71152f44
sameer-jahangir99 <sameerjahangier99@gmail.com>
2025-02-10 11:40:29 -0800

    Remove .next directory from tracking

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/a1989/Documents/projects/Furfy/src/app/layout\":[\"static/media/6905431624c34d00-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
